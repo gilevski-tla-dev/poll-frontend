2c03047fe2f8b623d1253354df4cbefc565bf84d
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2025-06-20 16:27:14 +0200

    ref

--- src/app/providers/app-init-provider.tsx
+++ src/app/providers/app-init-provider.tsx
@@ -1,5 +1,5 @@
+import { postAuth } from "@/entities/auth/api/post-auth";
 import { useAuthStore } from "@/entities/auth/model/store";
-import axios from "axios";
 import { useEffect, type ReactNode } from "react";
 
 interface Props {
@@ -7,32 +7,25 @@ interface Props {
 }
 
 export const AppInitProvider = ({ children }: Props) => {
-  const accessToken = useAuthStore((state) => state.accessToken);
-
   const setAccessToken = useAuthStore((state) => state.setAccessToken);
   const clearAccessToken = useAuthStore((state) => state.clearAccessToken);
   const initData = window.Telegram?.WebApp?.initData;
 
   useEffect(() => {
-    axios
-      .post("http://localhost:3000/api/auth", {
-        initData,
-      })
+    postAuth({ initData })
       .then((response) => {
-        setAccessToken(response.data.access_token);
+        setAccessToken(response.access_token);
       })
       .catch(() => {
         clearAccessToken();
       });
-    console.log("initData: ", initData);
-    console.log("accessToken: ", accessToken);
 
-    // return () => {
-    //   clearAccessToken();
-    // };
+    return () => {
+      clearAccessToken();
+    };
 
     // TODO Здесь будет происходить то что нужно при инициализации приложения
-  }, [accessToken, clearAccessToken, initData, setAccessToken]);
+  }, [clearAccessToken, initData, setAccessToken]);
 
   return <>{children}</>;
 };
